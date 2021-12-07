f33572594638ed232d4a648eb33ec6e6c903e9d4
James Petran <jm.ptrn@gmail.com>
2021-12-07 09:56:08 -0600

    2x for loops do the same thing w penguins

--- day02.js
+++ day02.js
@@ -31,3 +31,43 @@ let buildings = [
 ]
 console.log(`look at all the buildings:`);
 console.log(buildings);
+
+const penguins = [
+      {
+            name: `Emperor`,
+            isCute: true
+      },
+      {
+            name: `Little`,
+            isCute: true
+      },
+      {
+            name: `Sea Gull`,
+            isCute: false
+      }
+]
+
+function checkPenguinsFor(penguinsArray) {
+      let cutePenguins = [];
+      for (let i = 0; i < penguinsArray.length; i++) {
+            if (penguinsArray[i].isCute) {
+                  cutePenguins.push(penguinsArray[i].name)
+            }
+      }
+      return cutePenguins
+}
+function checkPenguinsForOf(penguinsArray) {
+      let cutePenguins = [];
+      for (let penguin of penguinsArray) {
+            if (penguin.isCute) {
+                  cutePenguins.push(penguin.name);
+            }
+      }
+      return cutePenguins
+}
+
+console.log(`all the cute penguins are:`, checkPenguinsFor(penguins));
+
+console.log(`all the cute penguins are:`, checkPenguinsForOf(penguins));
+
+//this means that for and for..of can do the same thing
