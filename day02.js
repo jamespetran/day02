ba791253f3c3c8bddef878dd977616c3a754c5f5
James Petran <jm.ptrn@gmail.com>
2021-12-07 09:45:48 -0600

    add  buildings

--- day02.js
+++ day02.js
@@ -1,9 +1,9 @@
-let snow ={
+let snow = {
       key: 'value',
       color: 'white',
       wet: true,
       inches: 4,
-      accidents: ['100','694','23','35','crosstown']
+      accidents: ['100', '694', '23', '35', 'crosstown']
 };
 
 let stringText = `Today is has already snowed ${snow.inches} inches`;
@@ -14,3 +14,20 @@ snow.iscold = true;
 
 console.log(`snow is`, snow);
 console.log(`snow is ${snow}`);
+
+let buildings = [
+      {
+            color: `brown`,
+            isTall: true
+      },
+      {
+            color: `gray`,
+            isTall: true
+      },
+      {
+            color: `blue`,
+            isTall: false
+      }
+]
+console.log(`look at all the buildings:`);
+console.log(buildings);
